da78ab8c705d03e080a77d217f39fcf060976efc
C-flo7 <93959560+C-flo7@users.noreply.github.com>
2022-09-24 20:35:23 -0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,22 +1,22 @@
 import java.awt.*;
 import javax.swing.*;
 import javax.swing.JTextField;
-public class Game1 extends Frame
+public class Game extends Frame
 {
   static int row;
   static int col;
   static JFrame fi;
   static JTextField input = new JTextField();
-  public Game1 (int r,int c)
+  public Game (int r,int c)
   {
     fi = new JFrame();
     row = r;
     col = c;
     fi.setSize(300,300);
     fi.setLayout(new GridLayout(10,10));
-    Box1 easy = new Box1("Easy");
-    Box1 medium = new Box1("Medium"); 
-    Box1 hard = new Box1("Hard"); 
+    Box easy = new Box("Easy");
+    Box medium = new Box("Medium"); 
+    Box hard = new Box("Hard"); 
     //fi.add(input); 
     fi.setBackground(Color.GRAY);
     fi.add(easy);
@@ -31,6 +31,6 @@ public class Game1 extends Frame
        size = 10;
      else
        size = input.getText();*/
-     new GridFrame1(row,col,difficulty);
+     new GridFrame(row,col,difficulty);
    }
 }
